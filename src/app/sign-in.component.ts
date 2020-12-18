e9780725ad83d3d1a230e359dc805a08ddc19577
Cao Minh <trancaominh1997@gmail.com>
2020-12-18 21:04:03 +0700

    form validate update

--- src/app/sign-in.component.ts
+++ src/app/sign-in.component.ts
@@ -3,19 +3,34 @@ import {Component} from "@angular/core";
 @Component({
   selector: 'app-sign-in',
   template: `
-    <form (submit)="onSubmit(SignInForm)" #SignInForm="ngForm">
-      <input type="text" [(ngModel)]="email" placeholder="Enter your username" name="username">
-      <br><br>
-      <input type="password" [(ngModel)]="password" placeholder="Enter your password" name="password">
-      <br><br>
-      <button>Submit</button>
+    <form (ngSubmit)="onSubmit(SignInForm)" #SignInForm="ngForm">
+      <tr>
+        <td>
+          <input type="text" ngModel #txtEmail = ngModel placeholder="Enter your username" name="email" required>
+        </td>
+        <td>
+          <p *ngIf="txtEmail.invalid && txtEmail.touched">
+            Email is invalid
+          </p>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <input type="password" ngModel #txtPassword = ngModel placeholder="Enter your password" name="password" required>
+        </td>
+        <td>
+          <p *ngIf="txtPassword.touched && txtPassword.invalid">
+            password is blank
+          </p>
+        </td>
+      </tr>
+      <button [disabled]="SignInForm.invalid">Submit</button>
     </form>
-  `
+  `,
+  styles: [`input.ng-invalid.ng-touched {border: 1px solid red}`]
 })
 
 export class SignInComponent {
-  email = '';
-  password = '';
   onSubmit(SignInForm){
     console.log(SignInForm);
   }
